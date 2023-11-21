ae1932d2999ab944dc682fa9d751a66aac498535
otyacazyu <vesperia.not.xperia@gmail.com>
2023-11-21 20:25:26 +0900

    post実装

--- src/main/java/com/example/gamelist/Controller.java
+++ src/main/java/com/example/gamelist/Controller.java
@@ -1,11 +1,14 @@
 package com.example.gamelist;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/controller")
 public class Controller {
 
     private final GameMapper gameMapper;
@@ -16,7 +19,14 @@ public class Controller {
 
     @GetMapping("/gamelist")
     public  List<GameName> gamelist(){
-        List<GameName> gamelists = gameMapper.findAll();
-                return gamelists;
+        return gameMapper.findAll();
+    }
+    @PostMapping("/gamelist")
+    public ResponseEntity<String> create(@RequestBody GameName form){
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/gamelist")
+                .build()
+                .toUri();
+        return ResponseEntity.ok("name successfully created");
     }
 }
