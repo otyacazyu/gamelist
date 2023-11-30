31023f9393447b24def473839c38c65b5ccb4b51
otyacazyu <vesperia.not.xperia@gmail.com>
2023-11-30 16:17:14 +0900

    行間の修正

--- src/main/java/com/example/gamelist/GameForm.java
+++ src/main/java/com/example/gamelist/GameForm.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 public class GameForm {
 
     private String name;
+
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+
     public GameForm(String name) {
         this.name = name;
     }
