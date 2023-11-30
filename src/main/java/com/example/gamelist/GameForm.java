e07755faf251872ea4a9965654f1f7dc7bd422c1
otyacazyu <vesperia.not.xperia@gmail.com>
2023-11-30 14:28:22 +0900

    @JsonCreatorを追加

--- src/main/java/com/example/gamelist/GameForm.java
+++ src/main/java/com/example/gamelist/GameForm.java
@@ -1,9 +1,11 @@
 package com.example.gamelist;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class GameForm {
 
     private String name;
-
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public GameForm(String name) {
         this.name = name;
     }
