7bbe0ba9bf1a470a94937336597034b236897d5c
otyacazyu <vesperia.not.xperia@gmail.com>
2023-12-16 14:53:57 +0900

    patch編集３

--- src/main/java/com/example/gamelist/GameController.java
+++ src/main/java/com/example/gamelist/GameController.java
@@ -1,7 +1,6 @@
 package com.example.gamelist;
 
 import Mapper.GameMapper;
-import Response.UpdateResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/gamelist/UpdateResponse.java
+++ src/main/java/com/example/gamelist/UpdateResponse.java
@@ -1,4 +1,4 @@
-package Response;
+package com.example.gamelist;
 
 public class UpdateResponse {
 
