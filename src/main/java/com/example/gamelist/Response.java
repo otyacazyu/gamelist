8137a9162f5b95b5b5793b89ffd5590fb86372ff
otyacazyu <vesperia.not.xperia@gmail.com>
2023-12-16 15:14:46 +0900

    patch編集５

--- src/main/java/com/example/gamelist/Response.java
+++ src/main/java/com/example/gamelist/Response.java
@@ -1,2 +1,13 @@
-package com.example.gamelist;public class Response {
+package com.example.gamelist;
+
+public class Response {
+    private String message;
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
