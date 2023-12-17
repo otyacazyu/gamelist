06196e8bbd3892fdd96b1fbe67b872b6bc8a8528
otyacazyu <vesperia.not.xperia@gmail.com>
2023-12-17 11:47:58 +0900

    postのGamelistServicwoに変更

--- src/main/java/com/example/gamelist/GameController.java
+++ src/main/java/com/example/gamelist/GameController.java
@@ -26,12 +26,12 @@ public class GameController {
 
 
     @PostMapping("/gamelist")
-    public ResponseEntity<GamelistService.Response> create(@RequestBody GameForm form){
+    public ResponseEntity<Response> create(@RequestBody GameForm form){
         URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
                 .path("/gamelist/id")
                 .build()
                 .toUri();
-        return ResponseEntity.created(url).body(new GamelistService.Response("name successfully created"));
+        return ResponseEntity.created(url).body(new GamelistService.Response("gamelist successfully created"));
     }
 
 
