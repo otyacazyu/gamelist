9edbf87a66cf7e147bc4e7bc372028447fb3f955
otyacazyu <vesperia.not.xperia@gmail.com>
2023-12-31 06:38:43 +0900

    ワイルドカードの設定と修正

--- src/main/java/com/example/gamelist/GameController.java
+++ src/main/java/com/example/gamelist/GameController.java
@@ -1,7 +1,12 @@
 package com.example.gamelist;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -33,8 +38,6 @@ public class GameController {
                 .toUri();
         return ResponseEntity.created(url).body(new Response("game successfully created"));
     }
-
-
 
     @PatchMapping("/gamelist/{id}")
     public ResponseEntity<UpdateResponse> UpdateGame(@PathVariable int id, @RequestBody UpdateRequest updateRequest) {
